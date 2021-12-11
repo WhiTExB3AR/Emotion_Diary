85e68bc676053fc34f452a7575236ee8587f63c5
Mercy.ThuyLe <62128227+WhiTExB3AR@users.noreply.github.com>
2021-12-11 20:31:51 +0700

    Update camera.py

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -1,12 +1,14 @@
 # -*- encoding: utf-8 -*-
 
 from apps.home import blueprint
-from flask import render_template, request, Response
+from flask import render_template, request
 from flask_login import login_required
 from jinja2 import TemplateNotFound
 
 # ------- Start: B3AR config code -------
+from flask import Response
 from apps.home.camera import gen_frames
+
 # ------- End: B3AR config code -------
 
 @blueprint.route('/index')
@@ -15,12 +17,13 @@ def index():
 
     return render_template('home/index.html', segment='index')
 
-
 # ------- Start: B3AR config code -------
+# @blueprint.route('/video_feed')
 def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-# ------- End: B3AR config code -------
 
+    res = Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return res
+# ------- End: B3AR config code -------
 
 @blueprint.route('/<template>')
 @login_required

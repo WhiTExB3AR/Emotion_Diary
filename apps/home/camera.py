dbc99be8818ebf92068638c7870114c76a40926f
Mercy.ThuyLe <62128227+WhiTExB3AR@users.noreply.github.com>
2021-12-11 16:43:51 +0700

    Update camera.py

--- apps/home/camera.py
+++ apps/home/camera.py
@@ -22,14 +22,15 @@ def gen_frames():  # generate frame by frame from camera
         if not success:
             break
         else:
-            print("[INFO] loading face detector...")
-            detector=os.path.dirname(cv2.__file__) + "/data/haarcascade_frontalface_default.xml"
-            faces=detector.detectMultiScale(frame,1.1,7)
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-             #Draw the rectangle around each face
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            ret, buffer = cv2.imencode('.jpg', frame)
+            # print("[INFO] loading face detector...") # face detection
+            # detector=os.path.dirname(cv2.__file__) + "/data/haarcascade_frontalface_default.xml"
+            # faces=detector.detectMultiScale(frame,1.1,7)
+            # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            #  #Draw the rectangle around each face
+            # for (x, y, w, h) in faces:
+            #     cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+            ret, buffer = cv2.imencode('.jpg', frame) # load video streaming
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')

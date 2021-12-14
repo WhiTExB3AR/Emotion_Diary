49b0ab027a9d8a9ae0a6c13851ba0792630fde64
Mercy.ThuyLe <62128227+WhiTExB3AR@users.noreply.github.com>
2021-12-14 11:47:05 +0700

    Create util.py for load trained model FMPN

--- apps/home/util.py
+++ apps/home/util.py
@@ -15,8 +15,6 @@
 
 # load trained model pytorch
 # https://www.youtube.com/watch?v=9L9jEOwRrCg&list=PLqnslRFeH2UrcDBWF5mfPGpqQDSta6VK4&index=17
-# https://towardsdatascience.com/build-a-web-application-for-predicting-apple-leaf-diseases-using-pytorch-and-flask-413f9fa9276a
-# https://towardsdatascience.com/create-an-image-classification-web-app-using-pytorch-and-streamlit-f043ddf00c24
 # https://www.python-engineer.com/posts/pytorch-model-deployment-with-flask/
 # https://www.analyticsvidhya.com/blog/2020/07/deploy-an-image-classification-model-using-flask/
 # https://developers.facebook.com/blog/post/2020/08/03/connecting-web-app-pytorch-model-using-amazon-sagemaker/
@@ -31,7 +29,6 @@
 # https://www.analyticsvidhya.com/blog/2020/09/integrating-machine-learning-into-web-applications-with-flask/
 # https://blog.paperspace.com/deploying-deep-learning-models-flask-web-python/
 # https://www.geeksforgeeks.org/deploy-machine-learning-model-using-flask/
-# https://towardsdatascience.com/deploying-machine-learning-models-into-a-website-using-flask-8582b7ce8802
 # https://blog.cambridgespark.com/deploying-a-machine-learning-model-to-the-web-725688b851c7
 # https://medium.com/swlh/how-to-create-an-interactive-machine-learning-web-application-using-flask-and-heroku-5ae76a45bfc5
-# https://towardsdatascience.com/build-a-machine-learning-web-app-in-python-683480acbd37
+# https://towardsdatascience.com/deploying-machine-learning-models-into-a-website-using-flask-8582b7ce8802

ae620f2174125089b81dce1e9416e493b7bf5f34
Mercy.ThuyLe <62128227+WhiTExB3AR@users.noreply.github.com>
2021-12-15 13:14:16 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 # -*- encoding: utf-8 -*-
 
+# ------- Start: B3AR config code -------
+import os
+import sys
+from flask import request
+from flask_restful import Resource, Api
+from apps.home.util import solver
+# ------- End: B3AR config code -------
+
 from flask_migrate import Migrate
 from sys import exit
 from decouple import config
@@ -13,6 +21,12 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 # The configuration
 get_config_mode = 'Debug' if DEBUG else 'Production'
 
+# ------- Start: B3AR config code -------
+# TODO Load Model
+# E load model hoặc solver gì đó ở bên util rồi gọi ở đây
+# ->solver
+# ------- End: B3AR config code -------
+
 try:
 
     # Load the configuration using the default values
@@ -23,6 +37,24 @@ except KeyError:
 
 app = create_app(app_config)
 Migrate(app, db)
+
+# ------- Start: B3AR config code -------
+# TODO đây là code mẫu tạo rest api, tạm để ở đây e thích dọn đi đâu thì tuỳ
+# Thử kết quả ở http://127.0.0.1:5000/get_fer?image=hahaha
+
+class GetFer(Resource):
+    def get(self):
+        image = request.args.get('image', 'no model')
+        response = {
+        	'success': True,
+        	'predicted_label': image + ' ---- ' + str(solver.__str__)
+        }
+        return response
+
+
+api = Api(app)
+api.add_resource(GetFer, '/get_fer')
+# ------- End: B3AR config code -------
 
 if DEBUG:
     app.logger.info('DEBUG       = ' + str(DEBUG))

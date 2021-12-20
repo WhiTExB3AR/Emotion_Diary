f99c4d05e3442fb59263cc0ecce99f1c1a4e6545
Mercy.ThuyLe <62128227+WhiTExB3AR@users.noreply.github.com>
2021-12-20 14:53:26 +0700

    Update camera.py

--- apps/home/camera.py
+++ apps/home/camera.py
@@ -34,6 +34,10 @@ def gen_frames():  # generate frame by frame from camera
             for (x, y, w, h) in faces:
                 # print("[INFO] Detected face...")
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 255), 2)
+                crop_face = gray[y:y+h, x:x+w] # cut only face in gray_scale
+
+                # emotion detection the cropped frame
+
             
             # encode OpenCV raw frame to jpg and displaying it
             ret, jpeg = cv2.imencode('.jpg', frame) 

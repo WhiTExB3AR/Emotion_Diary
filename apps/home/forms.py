8f851e61095c4359d2ab83fc8b25f6952615f779
Mercy.ThuyLe <62128227+WhiTExB3AR@users.noreply.github.com>
2021-12-25 18:30:47 +0700

    Update forms.py

--- apps/home/forms.py
+++ apps/home/forms.py
@@ -7,29 +7,55 @@ from wtforms.validators import DataRequired
 # create diary
 
 class CreateForm(FlaskForm):
-    post_datetime = TextField('Date Time',
-                                id='post_create_datetime',
-                                validators=[DataRequired()])
-    emoname = TextField('Mood today',
-                         id='emotion_create_diary',
-                         validators=[DataRequired()])
-    title = TextField('Title',
-                        id='title_create_diary')
-    content = TextField('Content',
-                    id='content_create_diary')    
+    post_datetime = TextField(
+        'Date Time',
+        id='post_create_datetime',
+        validators=[DataRequired()]
+    )
+    uid = TextField(
+        'User ID',
+        id='user_create_diary',
+        validators=[DataRequired()]
+    )
+    eid = TextField(
+        'Mood today',
+        id='eid_create_diary',
+        validators=[DataRequired()]
+    )
+    imgname = TextField(
+        'Image Name',
+        id='img_create_diary',
+        validators=[DataRequired()]
+    )
+    title = TextField(
+        'Title',
+        id='title_create_diary'
+    )
+    content = TextField(
+        'Content',
+        id='content_create_diary'
+    )    
 
 
 class ViewForm(FlaskForm):
-    post_datetime = DateTimeField('Date',
-                                id='post_view_datetime',
-                                validators=[DataRequired()])
-    emoname = TextField('Mood Today',
-                         id='today_view_emotion',
-                         validators=[DataRequired()])
-    title = TextField('Title',
-                        id='title_view_diary')
-    content = TextAreaField('Content',
-                    id='content_view_diary')    
+    post_datetime = DateTimeField(
+        'Date',
+        id='post_view_datetime',
+        validators=[DataRequired()]
+    )
+    emoname = TextField(
+        'Mood Today',
+        id='today_view_emotion',
+        validators=[DataRequired()]
+    )
+    title = TextField(
+        'Title',
+        id='title_view_diary'
+    )
+    content = TextAreaField(
+        'Content',
+        id='content_view_diary'
+    )    
 
 
 # ------- Start: B3AR config code -------
